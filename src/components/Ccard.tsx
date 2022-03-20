acb406203bee4c68a78d097afa87e0c5ea1da51d
Arslan <alijansoomro0@gmail.com>
2022-03-20 12:58:32 +0500

    Complete a portion of card

--- src/components/Ccard.tsx
+++ src/components/Ccard.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardHeader, IconButton, Button } from "@mui/material";
+import { Box, Card, CardHeader, IconButton, Divider, CardContent, List, ListItem, ListItemButton } from "@mui/material";
 import AddLinkIcon from '@mui/icons-material/AddLink';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useState } from "react";
@@ -9,7 +9,14 @@ const catOptions = [
     'Edit Category',
     'Open All Links',
     'Delete Category',
-    'Add Subcategory',
+]
+
+const links = [
+    'arslan-soomro.github.io',
+    'facebook.com',
+    'google.com',
+    'twitter.com',
+    'linuxilize.com',
 ]
 
 const Ccard = () => {
@@ -31,6 +38,17 @@ const Ccard = () => {
                 />
           </Box>
       } />
+      <Divider />
+      {/* TODO, Reduce the gap between links */}
+      <CardContent>
+          <List sx={{p: 0}}>
+              {links.map((item, ind) => (
+                <ListItem  key={ind}>
+                    <ListItemButton sx={{py: 1, m: 0, fontSize: 12}}>{item}</ListItemButton>
+                </ListItem>))}
+          </List>
+      </CardContent>
+
     </Card>
   );
 };
